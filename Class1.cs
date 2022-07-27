fb8174ee89cdb689f8bb2f4d3686e9998b6caa5c
sreedhar-gif <71756537+sreedhar-gif@users.noreply.github.com>
2022-07-27 15:34:06 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,167 @@
+﻿using DBFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBFirst
+{
+    internal class Class1
+    {
+        private static Emp addemployyedetails;
+
+        static void Main(string[] args)
+        {
+            GetAllEmpDetails();
+            Console.WriteLine();
+            GetAllEmpDetailsbydeptno();
+            GetAllEmpDetailsbyjob();
+            GetAllEmpDetailsbyempno();
+
+            AddEmployee();
+            deleteEmployee();
+            UpdateEmployee();
+            getstoredprocedue();
+            updatestoredprocedure();
+
+            addstoredprocedue();
+
+        }
+
+
+        public static void AddEmployee()
+        {
+            var ctx = new SreedharContext();
+            var t = new Emp { Empno = 1234, Ename = "Robert", Job = "ANALYST", Mgr = 7839, Hiredate = DateTime.Now, Sal = 3000, Comm = 200, Deptno = 20 };
+            ctx.Emps.Add(t);
+            ctx.SaveChanges();
+            var empDetails = ctx.Emps;
+            foreach (var emp in empDetails)
+            {
+                Console.WriteLine(emp.Ename + " " + emp.Job + " " + emp.Sal);
+            }
+        }
+
+        public static void deleteEmployee()
+        {
+            var ctx = new SreedharContext();
+            var deleteemployees = ctx.Emps.Where(emp => emp.Empno == 1234).SingleOrDefault();
+            ctx.Remove(deleteemployees);
+            Console.WriteLine("Employee deleted");
+            ctx.SaveChanges();
+        }
+
+        private static void UpdateEmployee()
+        {
+            var ctx=new SreedharContext();
+            var updateemployees = ctx.Emps.Where(emp => emp.Empno == 1234).SingleOrDefault();
+            updateemployees.Sal= 10;
+            //ctx.Emps.Update(updateemployees);
+            ctx.Update(updateemployees);
+            ctx.SaveChanges();
+            Console.WriteLine("Updated");
+          
+
+            //var employees = ctx.Emps;
+            //foreach (var employee in employees)
+            //{
+            //    Console.WriteLine(employee.Ename + "  " + employee.Sal+ "   " );
+            //}
+
+
+        }
+
+        private static void addstoredprocedue()
+        {
+            var ctx = new SreedharContext();
+            var employee = ctx.Emps.FromSqlRaw("Addnewemp @p0,@p1,@p2", 200, "nohit",40);
+            Console.WriteLine("Insert Sucessfull");
+            var employees = ctx.Emps;
+            ctx.SaveChanges();
+
+            foreach(var emp in employees)
+            {
+                Console.WriteLine(emp.Empno + "  " + emp.Ename);
+            }
+
+        }
+
+
+        private static void updatestoredprocedure()
+        {
+            var ctx = new SreedharContext();
+            var employees = ctx.Emps.FromSqlRaw("updateempname @p0,@p1", 1234, "sreedhar");
+            Console.WriteLine("Update sucessfull");
+            var employee = ctx.Emps;
+            ctx.SaveChanges();
+
+            foreach (var emp in employee)
+            {
+                Console.WriteLine(emp.Empno + "  " + emp.Ename);
+            }
+
+
+
+
+        }
+
+        private static void getstoredprocedue()
+        {
+            var ctx = new SreedharContext();
+            List <Emp> employees = ctx.Set<Emp>().FromSqlRaw("GetEmpDetails").ToList();
+
+            foreach (Emp emp in employees)
+            {
+                Console.WriteLine(emp.Ename + "  " + emp.Empno);
+            }
+          
+        }
+
+        private static void GetAllEmpDetails()
+        {
+            var ctx = new SreedharContext();
+            var employees = ctx.Emps;
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Ename + "  " + employee.Sal);
+            }
+        }
+        private static void GetAllEmpDetailsbydeptno()
+        {
+            var ctx = new SreedharContext();
+            var employees = ctx.Emps.Where(emp => emp.Deptno == 10);
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Ename);
+            }
+        }
+
+        private static void GetAllEmpDetailsbyjob()
+        {
+            var ctx = new SreedharContext();
+            var employees = ctx.Emps.Where(emp => emp.Job == "clerk");
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Ename);
+            }
+        }
+
+        private static void GetAllEmpDetailsbyempno()
+        {
+            var ctx = new SreedharContext();
+            var employees = ctx.Emps.Where(emp => emp.Empno == 7839);
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Ename);
+            }
+        }
+
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstApporach.Models;
+using CodeFirstApporach.Data;
+using codefirstapproach.Model;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LearnCodeFirstApproach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetAllBookDetails();
+            //AddBook();
+            //DeleteBook();
+            // UpdateBook();
+
+            // AddCustomerandOrder();
+
+            // GetAllCustomerandorder_Eagerloading();
+
+            //GetAllCustomerandorder_Explicitloading();
+            //jointables();
+            joinprocedure();
+
+
+            //DissconnectedArchitecture();
+        }
+        public static void AddBook()
+        {
+            var ctx = new BookContext();
+            var b1 = new Book { BookID = 1, BookName = "Harry Potter", author = "JK Rowling", price = 2000, BookAge = 5 };
+            var b2 = new Book { BookID = 2, BookName = "Mahabhrata", author = "Vedavyasa", price = 20000, BookAge = 10000 };
+            var b3 = new Book { BookID = 3, BookName = "Ramayana", author = "Valmiki", price = 15000, BookAge = 15000 };
+            var b4 = new Book { BookID = 4, BookName = "Ramacharita Manasa", author = "Kalidasa", price = 5000, BookAge = 1000 };
+            ctx.Books.Add(b1);
+            ctx.Books.Add(b2); ctx.SaveChanges();
+            ctx.Books.Add(b3); ctx.SaveChanges();
+            ctx.Books.Add(b4);
+            ctx.SaveChanges();
+            var BookDetails = ctx.Books;
+            foreach (var bk in BookDetails)
+            {
+                Console.WriteLine(bk.BookName + " " + bk.author + " " + bk.price);
+            }
+        }
+        public static void DeleteBook()
+        {
+            var ctx = new BookContext();
+            var deleteBook = ctx.Books.Where(x => x.BookName == "Harry Potter").SingleOrDefault();
+            ctx.Remove(deleteBook);
+            Console.WriteLine("Deleted");
+            ctx.SaveChanges();
+            var empDetails = ctx.Books;
+            var BookDetails = ctx.Books;
+            foreach (var bk in BookDetails)
+            {
+                Console.WriteLine(bk.BookName + " " + bk.author + " " + bk.price);
+            }
+        }
+        public static void UpdateBook()
+        {
+            var ctx = new BookContext();
+            var updateBook = ctx.Books.Where(x => x.BookID == 2).SingleOrDefault();
+            updateBook.BookName = "Vyasa Mahabharata";
+            ctx.Books.Update(updateBook);
+            Console.WriteLine("Updated");
+            ctx.SaveChanges();
+            var BookDetails = ctx.Books;
+            foreach (var bk in BookDetails)
+            {
+                Console.WriteLine(bk.BookName + " " + bk.author + " " + bk.price);
+            }
+
+        }
+
+        public static void GetAllBookDetails()
+        {
+            var ctx = new BookContext();
+            var BookDetails = ctx.Books;
+            foreach (var bk in BookDetails)
+            {
+                Console.WriteLine(bk.BookName + " " + bk.author + " " + bk.price);
+            }
+
+        }
+
+        public static void AddCustomerandOrder()
+        {
+            var ctx = new BookContext();
+            var Customer = new Customer();
+            Customer.ID = 3;
+            Customer.Name = "sreedhar";
+            Customer.Age = 22;
+
+            //var order=new Order();
+            //order.Order_ID = 1110;
+            //order.OrderDate = DateTime.Now;
+            //order.Amount = 1000;
+
+            //List<Order> myorders =new List<Order>();
+            //myorders.Add(order);
+            //Customer.Orders = myorders;
+
+            //order.cust = Customer;
+
+            //order.cust = ctx.Customers.Where(a => a.ID == 1).SingleOrDefault();
+
+
+            try
+            {
+                ctx.Customers.Add(Customer);
+
+                //ctx.Orders.Add(order);
+                ctx.SaveChanges();
+                Console.WriteLine("Customer and order is created");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.ToString());
+            }
+        }
+
+        //eager loading or early loading
+        public static void GetAllCustomerandorder_Eagerloading()
+        {
+            var ctx = new BookContext();
+            try
+            {
+                var Customers = ctx.Customers.Include(o => o.Orders);
+
+                foreach (var customer in Customers)
+                {
+                    Console.WriteLine(customer.Name);
+                    Console.WriteLine("----------");
+
+                    foreach (var order in customer.Orders)
+                    {
+                        Console.WriteLine(order.Amount + "  " + order.Order_ID);
+                    }
+                    Console.WriteLine("----------");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //Explicit loading
+        public static void GetAllCustomerandorder_Explicitloading()
+        {
+            var ctx = new BookContext();
+            try
+            {
+                var Customers = ctx.Customers;
+                foreach (var customer in Customers)
+                {
+                    Console.WriteLine(customer.Name);
+                    Console.WriteLine("----------");
+
+                    ctx.Entry(customer).Collection(o => o.Orders).Load();
+
+                    foreach (var order in customer.Orders)
+                    {
+                        Console.WriteLine(order.Amount.ToString() + "   " + order.OrderDate.ToString());
+                    }
+
+                    Console.WriteLine("----------");
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void jointables()
+        {
+            var ctx = new BookContext();
+            try
+            {
+                //    var result = ctx.Customers.GroupJoin(ctx.Orders, c => c.ID, o => o.cust.ID, (c, o) => new
+                //    {
+                //       c,
+                //       generic=o.FirstOrDefault()
+                //    });
+
+                //    foreach (var item in result)
+                //    {
+                //        Console.WriteLine(item.cust + " " + item.o);
+
+                //    }
+                //}
+                //catch (Exception ex)
+                //{
+                //    Console.WriteLine(ex.Message);
+                //}
+                var result = (from c in ctx.Customers
+                              join o in ctx.Orders on c.ID equals o.cust.ID
+                              select new
+                              {
+                                  Name = c.Name,
+                                  Price = o.Amount
+                              });
+                foreach (var item in result)
+                {
+                    Console.WriteLine(item.Name + " " + item.Price);
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        //public void jointableslambda()
+        //{
+        //    var data = new List<Data>();
+        //    var customerdetails = new List<CustomerList>();
+        //    var orderlist=new List<OrderList>();
+
+        //    var result = data.Join(customerdetails,
+        //        d => d.CUSTOMER_NAME,
+        //        g => g.ID,
+        //        (d, g) => new
+        //        {
+        //            d,
+        //            customer = g.FirstOrDefault()
+        //        }).GroupJoin(orderlist,
+        //        dd => dd.d.CUSTOMER_NAME,
+        //        i => i.cust,
+        //        (d, i) => new
+        //        {
+        //            data = d.d,
+        //            orderlist = i.FirstOrDefault()
+        //        });
+
+
+        //}
+
+        private static void DissconnectedArchitecture()
+        {
+            var ctx=new BookContext();
+            var customer = ctx.Customers.Where(c => c.ID == 1).SingleOrDefault();
+            ctx.Dispose();
+            UpdateCustomerName(customer);
+        }
+
+        private static void UpdateCustomerName(Customer customer)
+        {
+            var ctx =new BookContext();
+            customer.Name = "Peter";
+            Console.WriteLine(ctx.Entry(customer).State.ToString());
+
+            ctx.Attach(customer).State=EntityState.Modified;
+            ctx.SaveChanges();
+            Console.WriteLine("customer name is updated");
+
+        }
+
+        private static void joinprocedure()
+        {
+            var ctx=new BookContext();
+            List<CustData> datas = ctx.Set<CustData>().FromSqlRaw("jointables").ToList();
+            Console.WriteLine("Join Sucessfull");
+
+            foreach (var item in datas) 
+            {
+                Console.WriteLine(item.NAME+"  "+item.OrderAmt);
+            }          
+            
+        }
+    }
+}
